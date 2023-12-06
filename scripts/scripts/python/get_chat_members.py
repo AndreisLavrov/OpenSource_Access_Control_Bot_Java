f7ea2f3a27d2088ca11fffe5820496d68dcffc1b
Andrei <90472447+AndreisLavrov@users.noreply.github.com>
2023-12-06 17:15:51 +0300

    add config

--- scripts/scripts/python/get_chat_members.py
+++ scripts/scripts/python/get_chat_members.py
@@ -1,14 +1,10 @@
 from telethon import TelegramClient, events
-
-
-api_id = '28735038'
-api_hash = '2d057b2b9b1dbe212af5395b79e004de'
-phone_number = '79867824778'
+import config
 
 session_name = 'session'
 
 async def main():
-    async with TelegramClient(session_name, api_id, api_hash) as client:
+    async with TelegramClient(session_name, config.api_id, config.api_hash) as client:
         @client.on(events.ChatAction)
         async def handler(event):
             # Проверяем, добавлен ли бот в чат
@@ -20,7 +16,7 @@ async def main():
                 async for user in client.iter_participants(chat_id):
                     print(f"Участник чата: ID {user.id} Имя {user.first_name}")
 
-        await client.start(phone_number)
+        await client.start(config.phone_number)
         await client.run_until_disconnected()
 
 if __name__ == '__main__':
